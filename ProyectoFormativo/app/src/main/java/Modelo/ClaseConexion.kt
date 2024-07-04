0835ab289824df71275ab25d7a16d9533ed15a75
Edenilson <20200004@ricaldone.edu.sv>
2024-07-04 11:20:27 -0600

    Inicio del Fin

---
+++ ProyectoFormativo/app/src/main/java/Modelo/ClaseConexion.kt
@@ -0,0 +1,25 @@
+package Modelo
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class ClaseConexion {
+
+    fun cadenaConexion(): Connection?{
+
+        try {
+            val url = "jdbc:oracle:thin:@192.168.56.1:1521:xe"
+            val usuario = "SYSTEM"
+            val contrasena = "ITR2024"
+
+            val connection = DriverManager.getConnection(url,usuario,contrasena)
+            return connection
+
+        }
+
+        catch (e: Exception){
+            println("Este es el error: $e")
+            return null
+        }
+    }
+}
